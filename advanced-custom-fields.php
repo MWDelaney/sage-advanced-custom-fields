8bc2694323072cb2986189d49dc816bb42af49a7
Michael W. Delaney <michael.delaney@gmail.com>
2017-11-29 14:36:22 -0500

    Update advanced-custom-fields.php

--- advanced-custom-fields.php
+++ advanced-custom-fields.php
@@ -5,29 +5,40 @@ namespace App;
  * Version: 1.0
  * Author: Michael W. Delaney
  */
+
 /**
  * Set local json save path
  * @param  string $path unmodified local path for acf-json
  * @return string       our modified local path for acf-json
  */
+
 add_filter('acf/settings/save_json', function($path) {
+  
   // Set Sage9 friendly path at /theme-directory/resources/assets/acf-json
   $path = get_stylesheet_directory() . '/assets/acf-json';
+  
   // If the directory doesn't exist, create it.
   if (!is_dir($path)) {
     mkdir($path);
   }
+  
   // Always return
   return $path;
+  
 });
+
+
 /**
  * Set local json load path
  * @param  string $path unmodified local path for acf-json
  * @return string       our modified local path for acf-json
  */
 add_filter('acf/settings/load_json', function($paths) {
+  
   // append path
   $paths[] = get_stylesheet_directory() . '/assets/acf-json';
+  
   // return
   return $paths;
+  
 });
